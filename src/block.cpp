43bdcf711a31df1950b21d6084a536e144f89501
Adam Gmiterek <linuxuserftw@proton.me>
2024-12-02 09:28:22 +0100

    define block lists

---
+++ src/block.cpp
@@ -0,0 +1,5 @@
+#include "block.h"
+
+void defineBlock(Blcok* block) {
+    blockList.push_back(block);
+}
--- src/block.h
+++ src/block.h
@@ -1,10 +1,15 @@
 #ifndef BLOCK_H
 #define BLOCK_H
 
+#include <vector>
+
 class Block {
 private:
     int id;
 public:
+    Block();
 }
+
+extern std::vector<Block*> blockList;
 
 #endif
