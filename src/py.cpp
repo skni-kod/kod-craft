4c051e11872189e0968f736cf428343970d6274a
Adam Gmiterek <linuxuserftw@proton.me>
2024-12-02 11:38:52 +0100

    get data from python

--- src/py.cpp
+++ src/py.cpp
@@ -1,14 +1,30 @@
 #define PY_SSIZE_T_CLEAN
 #include<Python.h>
 
+#include "block.h"
+
 bool isPythonInitalized = false;
 
-static PyObject *py_createBlock(PyObject *self, PyObject *args) {
-    return PyLong_FromLong(12);
+
+static PyObject *py_createBlock(PyObject *self, PyObject *args, PyObject *kwargs) {
+    char *name;
+    int solid = 1;
+
+    static char *kwlist[] = {(char*)"name", (char*)"solid", NULL};
+
+    if (!PyArg_ParseTupleAndKeywords(args, kwargs,
+        "s|p", kwlist,
+        &name, &solid
+    )) return NULL;
+
+
+    Block block = Block();
+
+    return PyBool_FromLong(0);
 }
 
 static PyMethodDef pyMethods[] = {
-    {"defineBlock", py_createBlock, METH_VARARGS,
+    {"defineBlock", (PyCFunction)py_createBlock, METH_VARARGS | METH_KEYWORDS,
      "Define a block in the game to be used."},
     {NULL, NULL, 0, NULL}
 };
