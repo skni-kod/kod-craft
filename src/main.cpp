c2ca11b74c0f2917dc3a8db0a3965eb16fa9b1c7
Adam Gmiterek <linuxuserftw@proton.me>
2024-12-02 09:13:49 +0100

    gameState

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,18 @@
 
 #include<Python.h>
 
+enum GameState {
+    STATE_MAIN_MENU,
+    STATE_LOADING_GAME,
+    STATE_IN_GAME
+};
+
+GameState gameState;
+
 int main() {
     InitWindow(800, 450, "Kod Craft");
+
+    gameState = STATE_IN_GAME;
 
     while (!WindowShouldClose()){
         BeginDrawing();
