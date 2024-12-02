40aaf2bd6223684f3f7798ab2ebc976714423f4c
Adam Gmiterek <linuxuserftw@proton.me>
2024-12-02 09:14:00 +0100

    dimension class

---
+++ src/dimension.h
@@ -0,0 +1,14 @@
+#ifndef DIMENSION_H
+#define DIMENSION_H
+
+#include<vector>
+
+class Dimension {
+private:
+    std::vector<Chunk*> chunks;
+    int chunkSize;
+public:
+    Dimension(int chunkSize);
+}
+
+#endif
