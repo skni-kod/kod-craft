29f30e999bdf6716ac7f6d0c020b84ddf18ff2f0
Adam Gmiterek <linuxuserftw@proton.me>
2024-12-09 09:23:17 +0100

    Dimension constructor

--- src/dimension.cpp
+++ src/dimension.cpp
@@ -2,6 +2,12 @@
 
 std::vector<DimensionTemplate*> dimensionList;
 
+Dimension::Dimension(int type) {
+    this->propeties = dimensionList[type];
+
+    this->chunkSize = this->propeties.chunkSize;
+}
+
 DimensionTemplate::DimensionTemplate(int chunkSize, std::string name) {
     this->chunkSize = chunkSize;
     this->name = name;
