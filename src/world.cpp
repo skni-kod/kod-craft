9dd442e67f17f94297afbc8a7e747817c2f39d14
Adam Gmiterek <linuxuserftw@proton.me>
2024-12-09 09:02:32 +0100

    create new world

--- src/world.cpp
+++ src/world.cpp
@@ -2,10 +2,14 @@
 #include "states.h"
 #include "world.h"
 
+World::World() {}
+
 World * world;
 
 void loadWorld() {
     gameState = STATE_LOADING_GAME;
+
+    world = new World();
 
     gameState = STATE_IN_GAME;
 }
