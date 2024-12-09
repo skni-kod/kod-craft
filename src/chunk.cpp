7836dad07703ae23ffe4b1bf6a3f616d7f5b896a
Adam Gmiterek <linuxuserftw@proton.me>
2024-12-09 14:53:19 +0100

    get blocks from python array

--- src/chunk.cpp
+++ src/chunk.cpp
@@ -10,17 +10,24 @@ Chunk::Chunk(Dimension* dimension, int x, int y, int z) {
 
     int chunkSize = dimension->getChunkSize();
 
+    PyObject* generateChunkCallback = dimension->getTemplate()->generateChunkCallback;
+    PyObject* chunkArray = PyObject_CallObject(generateChunkCallback, Py_BuildValue("(iiii)", x, y, z, chunkSize));
 
     // allocate memory
     this->blocks = (Block***) malloc(chunkSize * sizeof(Block**));
 
     for (int bX = 0; bX < chunkSize; bX++) {
         this->blocks[bX] = (Block**) malloc(chunkSize * sizeof(Block*));
+        PyObject* chunkArrayX = PyList_GetItem(chunkArray, bX);
+
         for (int bY = 0; bY < chunkSize; bY++) {
             this->blocks[bX][bY] = (Block*) malloc(chunkSize * sizeof(Block));
+            PyObject* chunkArrayXY = PyList_GetItem(chunkArrayX, bY);
 
-            for (int bY = 0; bY < chunkSize; bY++) {
-                
+            for (int bZ = 0; bZ < chunkSize; bZ++) {
+                PyObject* chunkBlock = PyList_GetItem(chunkArrayXY, bZ);
+
+                this->blocks[bX][bY][bZ] = Block(PyUnicode_AsUTF8(chunkBlock));
             }
         }
     }
