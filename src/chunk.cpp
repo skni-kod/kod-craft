087c3bd82aee9a3e280bf45f3a31893088efb181
Kuba663 <kuba307@poczta.onet.pl>
2024-12-17 20:22:40 +0100

    Przepisanie tablicy Bloków w Chunk'u na pamięć jednolitą
    Signed-off-by: Kuba663 <kuba307@poczta.onet.pl>

--- src/chunk.cpp
+++ src/chunk.cpp
@@ -1,4 +1,3 @@
-
 #include "chunk.h"
 
 Chunk::Chunk(Dimension* dimension, int x, int y, int z) {
@@ -14,20 +13,18 @@ Chunk::Chunk(Dimension* dimension, int x, int y, int z) {
     PyObject* chunkArray = PyObject_CallObject(generateChunkCallback, Py_BuildValue("(iiii)", x, y, z, chunkSize));
 
     // allocate memory
-    this->blocks = (Block***) malloc(chunkSize * sizeof(Block**));
+    this->blocks.reserve(chunkSize*chunkSize*chunkSize);
 
     for (int bX = 0; bX < chunkSize; bX++) {
-        this->blocks[bX] = (Block**) malloc(chunkSize * sizeof(Block*));
         PyObject* chunkArrayX = PyList_GetItem(chunkArray, bX);
 
         for (int bY = 0; bY < chunkSize; bY++) {
-            this->blocks[bX][bY] = (Block*) malloc(chunkSize * sizeof(Block));
             PyObject* chunkArrayXY = PyList_GetItem(chunkArrayX, bY);
 
             for (int bZ = 0; bZ < chunkSize; bZ++) {
                 PyObject* chunkBlock = PyList_GetItem(chunkArrayXY, bZ);
 
-                this->blocks[bX][bY][bZ] = Block(PyUnicode_AsUTF8(chunkBlock));
+                this->blocks[(bZ*(chunkSize*chunkSize))+(bY*chunkSize)+bX] = Block(PyUnicode_AsUTF8(chunkBlock));
             }
         }
     }
