522a15ccc8fd4833f9e3a362551dd2f9cc214967
Kuba663 <kuba307@poczta.onet.pl>
2024-12-17 20:27:56 +0100

    Funkcja wybioru bloków z tablicy
    Signed-off-by: Kuba663 <kuba307@poczta.onet.pl>

--- src/chunk.h
+++ src/chunk.h
@@ -16,6 +16,11 @@ private:
     Dimension* dimension;
     std::vector<Block> blocks;
 
+    inline Block& getBlock(int x, int y, int z) {
+        auto chunkSize = dimension->getChunkSize();
+        return this->blocks[(z * chunkSize * chunkSize) + (y * chunkSize) + x];
+    }
+
     int x, y, z;
 public:
     Chunk(Dimension* dimension, int x, int y, int z);
