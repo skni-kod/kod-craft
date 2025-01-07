34eff6b9a6542eb72e23cede17ac57e35aa27696
Adam Gmiterek <linuxuserftw@proton.me>
2025-01-07 15:57:49 +0100

    check game states

--- src/world.cpp
+++ src/world.cpp
@@ -25,6 +25,9 @@ void processTicksThreadFunction(World * world) {
 }
 
 void World::startTickProcessing() {
+    if (gameState == STATE_LOADING_GAME) gameState = STATE_IN_GAME;
+    else return;
+    
     tickProcessingTrhead = std::thread(&processTicksThreadFunction, this);
 }
 
