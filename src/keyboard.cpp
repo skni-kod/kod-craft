ebbba3f0cdcd07f399562bb351be363aba4c3131
Adam Gmiterek <linuxuserftw@proton.me>
2025-03-17 13:00:50 +0100

    keyboard functions

--- src/keyboard.cpp
+++ src/keyboard.cpp
@@ -1,2 +1,18 @@
 #include "keyboard.h"
 
+Keyboard::Keyboard(int keyCode) {
+    this->keyCode = keyCode;
+    this->state = KEY_DEPRESSED;
+}
+
+void Keyboard::onPressed() {
+    this->state = KEY_PRESSED;
+}
+
+void Keyboard::onDepressed() {
+    this->state = KEY_DEPRESSED;
+}
+
+bool Keyboard::getState() {
+    return this->state;
+}
--- src/keyboard.h
+++ src/keyboard.h
@@ -1,8 +1,17 @@
 #pragma once
 
+const bool KEY_PRESSED = true;
+const bool KEY_DEPRESSED = false;
+
 class Keyboard {
+    int keyCode;
+    bool state;
 public:
-    Keyboard();
+    Keyboard(int keyCode);
     ~Keyboard();
     
+    void onPressed();
+    void onDepressed();
+
+    bool getState();
 };
