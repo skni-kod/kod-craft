c5a6e62f2dca9d25597216c6f203d6a71491eead
Adam Gmiterek <linuxuserftw@proton.me>
2025-03-17 13:14:54 +0100

    define python keyboard functions

--- src/keyboard.cpp
+++ src/keyboard.cpp
@@ -1,18 +1,36 @@
+#include<Python.h>
+#define USE_PYTHON
+
 #include "keyboard.h"
 
-Keyboard::Keyboard(int keyCode) {
+KeyboardKey::KeyboardKey(int keyCode) {
     this->keyCode = keyCode;
     this->state = KEY_DEPRESSED;
 }
 
-void Keyboard::onPressed() {
+void KeyboardKey::onPressed() {
     this->state = KEY_PRESSED;
 }
 
-void Keyboard::onDepressed() {
+void KeyboardKey::onDepressed() {
     this->state = KEY_DEPRESSED;
 }
 
-bool Keyboard::getState() {
+bool KeyboardKey::getState() {
     return this->state;
+}
+
+int pyInitEntity(py_KeyboardKeyClass* self, PyObject* args, PyObject* kwargs) {
+    int keyCode;
+
+    static char *kwlist[] = {(char*)"keyCode", NULL};
+
+    if (!PyArg_ParseTupleAndKeywords(args, kwargs,
+        "i", kwlist,
+        &keyCode
+    )) return -1;
+
+
+    self->instance = new KeyboardKey(keyCode);
+    return 0;
 }
--- src/keyboard.h
+++ src/keyboard.h
@@ -3,15 +3,37 @@
 const bool KEY_PRESSED = true;
 const bool KEY_DEPRESSED = false;
 
-class Keyboard {
+class KeyboardKey {
     int keyCode;
     bool state;
 public:
-    Keyboard(int keyCode);
-    ~Keyboard();
+    KeyboardKey(int keyCode);
+    ~KeyboardKey();
     
     void onPressed();
     void onDepressed();
 
     bool getState();
 };
+
+#ifdef PYTHON_DEFINTION
+#define USE_PYTHON
+#endif
+
+#ifdef USE_PYTHON
+
+struct py_KeyboardKeyClass {
+    PyObject_HEAD
+    KeyboardKey* instance;
+};
+
+#endif
+
+
+#ifdef PYTHON_DEFINTION
+
+PyTypeObject py_KeyboardKeyClassType;
+
+int pyInitKeyboardKey(py_KeyboardKeyClass* self, PyObject* args, PyObject* kwargs);
+
+#endif
